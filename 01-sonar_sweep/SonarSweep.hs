d0ba21939024ef6fa59a57ea0e6ea11c7d48ff3a
Scriptim <Scriptim@gmx.de>
2021-12-01 18:01:59 +0100

    01.2

--- 01-sonar_sweep/SonarSweep.hs
+++ 01-sonar_sweep/SonarSweep.hs
@@ -1,10 +1,19 @@
-module SonarSweep (parseInput, part1) where
+module SonarSweep (parseInput, part1, part2) where
+
+import Control.Monad (ap)
+import Data.List (tails)
 
 numIncreases :: Ord a => [a] -> Int
 numIncreases = length . filter (LT ==) . ap (zipWith compare) tail
 
+windows :: (Num a) => [a] -> [a]
+windows = map (sum . take 3) . tails
+
 part1 :: [Integer] -> String
 part1 = show . numIncreases
+
+part2 :: [Integer] -> String
+part2 = part1 . windows
 
 parseInput :: String -> [Integer]
 parseInput = map read . lines
