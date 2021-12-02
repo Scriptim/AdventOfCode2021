3643c71f15a33affde3ad009968c980a1730c86f
Scriptim <Scriptim@gmx.de>
2021-12-02 21:00:51 +0100

    02.2

--- 02-dive/Dive.hs
+++ 02-dive/Dive.hs
@@ -1,4 +1,4 @@
-module Dive (parseInput, part1) where
+module Dive (parseInput, part1, part2) where
 
 import Control.Arrow ((***))
 import Data.Char (toUpper)
@@ -11,8 +11,17 @@ dive1 (x, d) ((Forward, units) : xs) = dive1 (x + units, d) xs
 dive1 (x, d) ((Down, units) : xs) = dive1 (x, d + units) xs
 dive1 (x, d) ((Up, units) : xs) = dive1 (x, d - units) xs
 
+dive2 :: (Integer, Integer, Integer) -> [(Direction, Integer)] -> (Integer, Integer, Integer)
+dive2 pos [] = pos
+dive2 (x, d, a) ((Forward, units) : xs) = dive2 (x + units, d + a * units, a) xs
+dive2 (x, d, a) ((Down, units) : xs) = dive2 (x, d, a + units) xs
+dive2 (x, d, a) ((Up, units) : xs) = dive2 (x, d, a - units) xs
+
 part1 :: [(Direction, Integer)] -> String
 part1 = show . uncurry (*) . dive1 (0, 0)
+
+part2 :: [(Direction, Integer)] -> String
+part2 = show . (\(x, d, _) -> x * d) . dive2 (0, 0, 0)
 
 parseInput :: String -> [(Direction, Integer)]
 parseInput = map ((read *** read) . span (' ' /=) . capitalize) . lines
