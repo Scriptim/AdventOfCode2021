9066eb4ea2910c57c5dcc4f75606f9ff24378520
Scriptim <Scriptim@gmx.de>
2021-12-03 21:52:06 +0100

    03.2

--- 03-binary_diagnostic/BinaryDiagnostic.hs
+++ 03-binary_diagnostic/BinaryDiagnostic.hs
@@ -1,4 +1,4 @@
-module BinaryDiagnostic (parseInput, part1) where
+module BinaryDiagnostic (parseInput, part1, part2) where
 
 import Control.Applicative (liftA2)
 import Data.List (foldl', group, maximumBy, sort, transpose)
@@ -13,8 +13,16 @@ mostCommonElems = map (head . maximumBy (comparing length) . group . sort) . tra
 bitNot :: String -> String
 bitNot = map (\bit -> if bit == '0' then '1' else '0')
 
+iterateMatch :: Eq a => ([[a]] -> [a]) -> [[a]] -> [a]
+iterateMatch criterion = head . snd . until ((== 1) . length . snd) iterator . (,) 0
+  where
+    iterator (n, xs) = (n + 1, filter ((criterion xs !! n ==) . (!! n)) xs)
+
 part1 :: [String] -> String
 part1 = show . liftA2 (*) bin2dec (bin2dec . bitNot) . mostCommonElems
+
+part2 :: [String] -> String
+part2 xs = show $ bin2dec (iterateMatch mostCommonElems xs) * bin2dec (iterateMatch (bitNot . mostCommonElems) xs)
 
 parseInput :: String -> [String]
 parseInput = lines
